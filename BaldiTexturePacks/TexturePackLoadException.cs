24e632364846397ed21e5e96fb13b745809d38e6
benjaminpants <maltbyfamily@gmail.com>
2024-03-05 16:58:17 -0800

    Update TexturePackLoadException.cs

--- BaldiTexturePacks/TexturePackLoadException.cs
+++ BaldiTexturePacks/TexturePackLoadException.cs
@@ -11,6 +11,7 @@ namespace BaldiTexturePacks
         public TexturePackLoadException(TexturePack pack, string message)
         {
             _message = message;
+            _pack = pack;
         }
 
         public override string Message => String.Format("({0}) {1}", _pack.Name, _message);
